4accfa8e5416cdd1ca646e238059d8e4e575158e
Vladimir Kalinichev <wrumly@gmail.com>
2016-11-12 19:56:35 +0300

    Pixeled animation for photo

--- src/components/BusinessCard.js
+++ src/components/BusinessCard.js
@@ -1,15 +1,15 @@
 import CSSModules from 'react-css-modules'
 import * as styles from './BusinessCard.styl'
-import photo from './Vladimir.Kalinichev.jpg'
+//import photo from './Vladimir.Kalinichev.jpg'
 import SocialIcons from './SocialIcons/SocialButtons'
 
 const BusinessCard = () =>
 
     <div styleName='card' >
-        <img styleName='photo' src={ photo } />
+        <figure styleName='photo' />
         <div styleName='info'>
             <h3 styleName='title'>Vladimir Kalinichev</h3>
-            <div styleName='post'>JavaScript-developer</div>
+            <div styleName='post'>JavaScript Developer</div>
             <SocialIcons/>
         </div>
     </div>
